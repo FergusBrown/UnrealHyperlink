2972658a70d22fc263a0ce06aee3efdd16cfdc06
FergusBrown <fergus.brown97@gmail.com>
2023-11-24 09:00:00 +0100

    Copy node link functionality

--- UnrealHyperlink/Source/HyperlinkEditor/Private/HyperlinkNode.cpp
+++ UnrealHyperlink/Source/HyperlinkEditor/Private/HyperlinkNode.cpp
@@ -3,8 +3,29 @@
 
 #include "HyperlinkNode.h"
 
+#include "GraphEditorModule.h"
 #include "HyperlinkFormat.h"
 #include "HyperlinkUtils.h"
+#include "Log.h"
+
+#define LOCTEXT_NAMESPACE "HyperlinkNode"
+
+FHyperlinkNodeCommands::FHyperlinkNodeCommands()
+	: TCommands<FHyperlinkNodeCommands>(
+		TEXT("HyperlinkNode"),
+		NSLOCTEXT("Contexts", "HyperlinkNode", "Hyperlink Node"),
+		NAME_None,
+		FAppStyle::GetAppStyleSetName())
+{
+}
+
+void FHyperlinkNodeCommands::RegisterCommands()
+{
+	UI_COMMAND(CopyNodeLink, "Copy Node Link", "Copy a link to the selected node in the graph editor",
+		EUserInterfaceActionType::Button, FInputChord());
+}
+
+#undef LOCTEXT_NAMESPACE
 
 UHyperlinkNode::UHyperlinkNode()
 {
@@ -16,21 +37,60 @@ UHyperlinkNode::UHyperlinkNode()
 
 void UHyperlinkNode::Initialize()
 {
+	FHyperlinkNodeCommands::Register();
+	NodeCommands = MakeShared<FUICommandList>();
+	NodeCommands->MapAction(
+		FHyperlinkNodeCommands::Get().CopyNodeLink,
+		FExecuteAction::CreateUObject(this, &UHyperlinkDefinition::CopyLink)
+	);
+	
+	FGraphEditorModule& GraphEditor{ FModuleManager::LoadModuleChecked<FGraphEditorModule>(TEXT("GraphEditor")) };
+	// TODO: See if it's possible to add keyboard shortcut to graph editor. Would have to be added SGraphEditor on creation
+	FGraphEditorModule::FGraphEditorMenuExtender_SelectedNode SelectedNodesDelegate
+	{
+		FGraphEditorModule::FGraphEditorMenuExtender_SelectedNode::CreateLambda([=](const TSharedRef<FUICommandList>,
+			const UEdGraph*, const UEdGraphNode* Node, const UEdGraphPin*, bool)
+			{
+				SelectedNode = Node;
+				return FHyperlinkUtils::GetMenuExtender(TEXT("EdGraphSchemaNodeActions"), EExtensionHook::After,
+					NodeCommands, FHyperlinkNodeCommands::Get().CopyNodeLink, TEXT("CopyNodeLink"));
+			})
+	};
 
+	NodeContextMenuHandle = SelectedNodesDelegate.GetHandle();
+	GraphEditor.GetAllGraphEditorContextMenuExtender().Emplace(MoveTemp(SelectedNodesDelegate));
 }
 
 void UHyperlinkNode::Deinitialize()
 {
-
+	FGraphEditorModule& GraphEditor{ FModuleManager::LoadModuleChecked<FGraphEditorModule>(TEXT("GraphEditor")) };
+	GraphEditor.GetAllGraphEditorContextMenuExtender().RemoveAll(
+		[=](const FGraphEditorModule::FGraphEditorMenuExtender_SelectedNode& Delegate){ return Delegate.GetHandle() == NodeContextMenuHandle; });
+	
+	FHyperlinkNodeCommands::Unregister();
 }
 
 bool UHyperlinkNode::GenerateLink(FString& OutLink) const
 {
-	bool bSuccess{ false };
+	const bool bSuccess{ SelectedNode.IsValid() && SelectedNode->GetGraph() };
+	
+	if (bSuccess)
+	{
+		const FString AssetPackageName{ SelectedNode->GetGraph()->GetOuter()->GetPackage()->GetName() };
+		const FGuid& NodeGuid{ SelectedNode->NodeGuid };
+		OutLink = GenerateLink(AssetPackageName, NodeGuid);
+	}
+	else
+	{
+		UE_LOG(LogHyperlinkEditor, Display, TEXT("Cannot generate Node link: no graph editor node is selected."));
+	}
 
-	
-	
 	return bSuccess;
+}
+
+FString UHyperlinkNode::GenerateLink(const FString& AssetPackageName, const FGuid& NodeGuid) const
+{
+	return GetHyperlinkBase() / AssetPackageName + FHyperlinkFormat::ArgSeparator + NodeGuid.ToString();
 }
 
 void UHyperlinkNode::ExecuteLinkBodyInternal(const TArray<FString>& LinkArguments)
@@ -40,6 +100,7 @@ void UHyperlinkNode::ExecuteLinkBodyInternal(const TArray<FString>& LinkArgument
 
 	FHyperlinkUtils::OpenEditorForAsset(PackageName);
 
+	// TODO
 	// Need to ensure the graph editor is opened for the asset
 	// This needs to be handled differently for different asset types
 }
--- UnrealHyperlink/Source/HyperlinkEditor/Public/HyperlinkNode.h
+++ UnrealHyperlink/Source/HyperlinkEditor/Public/HyperlinkNode.h
@@ -6,6 +6,16 @@
 #include "HyperlinkDefinition.h"
 #include "HyperlinkNode.generated.h"
 
+class FHyperlinkNodeCommands : public TCommands<FHyperlinkNodeCommands>
+{
+public:
+	FHyperlinkNodeCommands();
+	virtual void RegisterCommands() override;
+
+public:
+	TSharedPtr<FUICommandInfo> CopyNodeLink{ nullptr };
+};
+
 /**
  * Hyperlink for opening a graph editor and focusing on a node
  */
@@ -20,7 +30,14 @@ public:
 	virtual void Initialize() override;
 	virtual void Deinitialize() override;
 	virtual bool GenerateLink(FString& OutLink) const override;
+	FString GenerateLink(const FString& AssetPackageName, const FGuid& NodeGuid) const;
 
 protected:
 	virtual void ExecuteLinkBodyInternal(const TArray<FString>& LinkArguments) override;
+
+private:
+	TSharedPtr<FUICommandList> NodeCommands{};
+	FDelegateHandle NodeContextMenuHandle{};
+	
+	TWeakObjectPtr<const UEdGraphNode> SelectedNode{ nullptr };
 };
