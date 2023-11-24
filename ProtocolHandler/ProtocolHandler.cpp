ca2a73124061dc86be997259a640e24d1b492dd8
FergusBrown <fergus.brown97@gmail.com>
2023-11-24 09:00:00 +0100

    Updated protocol handler to use wchars to try and ensure same encoding as unreal strings

--- ProtocolHandler/ProtocolHandler.cpp
+++ ProtocolHandler/ProtocolHandler.cpp
@@ -1,34 +1,36 @@
+#define UNICODE // Ensure we're using unicode string encoding to match unreal windows default
+
 #include <iostream>
 #include <regex>
-#include "windows.h"
+#include <windows.h>
 
-int main(int argc, char* argv[])
+int wmain(int argc, TCHAR* argv[])
 {
     if (argc != 2)
     {
-        std::cout << "Provided " << argc - 1 << " arguments, expected 1 argument." << std::endl;
+        std::wcout << L"Provided " << argc - 1 << L" arguments, expected 1 argument." << std::endl;
         return EXIT_FAILURE;
     }
 
-    const std::string link{ argv[1] };
-    const std::regex projectPattern{ R"(^unreal:\/\/(\w+)\/.*)" };
+    const std::wstring link{ argv[1] };
+    const std::wregex projectPattern{ LR"(^unreal:\/\/(\w+)\/.*)" };
 
-    std::smatch match{};
+    std::wsmatch match{};
     
     if (!std::regex_match(link, match, projectPattern))
     {
-        std::cout << "Provided link is not in the format unreal://PROJECT_NAME/..." << std::endl;
+        std::wcout << L"Provided link is not in the format unreal://PROJECT_NAME/..." << std::endl;
         return EXIT_FAILURE;
     }
 
     // Create pipe name in format \\.\pipe\PipeName
     // Add "Link" to the pipe name to avoid potential conflicts with other services
-    std::string pipeName{ R"(\\.\pipe\)" + match.str(1) + "Link" };
+    std::wstring pipeName{ LR"(\\.\pipe\)" + match.str(1) + L"Link" };
 
     // Connect to pipe server
     HANDLE pipeHandle
     { 
-        CreateFileA(
+        CreateFileW(
             pipeName.c_str(),       // pipe name
             GENERIC_WRITE,          // desired access
             0,                      // default share more
@@ -42,18 +44,18 @@ int main(int argc, char* argv[])
     // Don't wait for busy pipe as this could result in confusing behaviour
     if (pipeHandle == INVALID_HANDLE_VALUE)
     {
-        std::cout << "Failed to connect to pipe server " << pipeName << " GLE = " << GetLastError() << std::endl;
+        std::wcout << L"Failed to connect to pipe server " << pipeName << L" GLE = " << GetLastError() << std::endl;
         return EXIT_FAILURE;
     }
 
     // Send message to pipe server
-    const LPCSTR message{ link.c_str() };
-    const DWORD bufferSize{ static_cast<DWORD>((strlen(message) + 1) * sizeof(char)) }; // +1 is for \0 terminator
+    const TCHAR* message{ link.c_str() };
+    const DWORD bufferSize{ static_cast<DWORD>((link.length() + 1) * sizeof(TCHAR)) }; // +1 is for \0 terminator
     DWORD bytesWritten{};
 
     if (!WriteFile(pipeHandle, message, bufferSize, &bytesWritten, nullptr))
     {
-        std::cout << "Failed to write to pipe server " << pipeName << " GLE = " << GetLastError() << std::endl;
+        std::wcout << L"Failed to write to pipe server " << pipeName << L" GLE = " << GetLastError() << std::endl;
         return EXIT_FAILURE;
     }
 
