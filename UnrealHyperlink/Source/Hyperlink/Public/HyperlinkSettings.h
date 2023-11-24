95b4d61a7f9f911fe2c0565fe1fe4d956cc547c1
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2023-11-24 09:00:00 +0100

    Remove settings properties intended for use with old link handling method

--- UnrealHyperlink/Source/Hyperlink/Public/HyperlinkSettings.h
+++ UnrealHyperlink/Source/Hyperlink/Public/HyperlinkSettings.h
@@ -6,24 +6,6 @@
 #include "Engine/DeveloperSettings.h"
 #include "HyperlinkSettings.generated.h"
 
-class UHyperlinkDefinition;
-
-UENUM()
-enum class EHyperlinkHandlingMethod : uint8
-{
-	/* Use the local "unreal://" url scheme */
-	Local,
-	
-	/*
-	 * Use the specified web server to handle links.
-	 * This will involve querying a web server which redirects to the local "unreal://" link.
-	 * While this method has an added intermediate stage it has the advantage that generated links
-	 * will be treated as regular web links allowing you to past the hyperlinks in applications
-	 * which block the "unreal://" link type
-	 */
-	Web,		
-};
-
 UCLASS(Config = Hyperlink, DefaultConfig, meta = (DisplayName = "Hyperlink"))
 class HYPERLINK_API UHyperlinkSettings : public UDeveloperSettings
 {
@@ -40,12 +22,7 @@ public:
 	TConstArrayView<FHyperlinkClassEntry> GetRegisteredDefinitions() const;
 
 	const FString& GetProjectIdentifier() const{ return ProjectIdentifier; };
-	EHyperlinkHandlingMethod GetLinkHandlingMethod()const { return LinkHandlingMethod; };
-	const FString& GetLinkHandlerAddress() const{ return LinkHandlerAddress; };
 	uint32 GetLocalServerPort() const{ return LocalServerPort; };
-	
-	/* Use for versioning of the http link address */
-	static constexpr uint32 LinkVersion{ 1 };
 	
 #if WITH_EDITOR
 private:
@@ -65,14 +42,6 @@ protected:
 	/* Project identifier used in the link. By default this should be the name of the project. */
 	UPROPERTY(Config, EditAnywhere, Category = "Project")
 	FString ProjectIdentifier{ TEXT("") };
-
-	/* What method should be used to generate and handle links */
-	UPROPERTY(Config, EditAnywhere, Category = "Project")
-	EHyperlinkHandlingMethod LinkHandlingMethod{ EHyperlinkHandlingMethod::Web };
-
-	/* Web address which is used to handle links */
-	UPROPERTY(Config, EditAnywhere, Category = "Project", meta = (EditCondition = "HandlingMethod == EHyperlinkHandlingMethod::Web"))
-	FString LinkHandlerAddress{ TEXT("uehyper.link") };
 
 	/** The port of the web server used for handling links. */
 	UPROPERTY(config, EditAnywhere, Category = "Project")
